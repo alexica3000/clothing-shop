04f9a37f6ab28920b4bd04882491982599fce036
Alex S <sanatateacom@gmail.com>
2021-03-17 10:09:55 +0200

    added spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,11 +3,41 @@ import {Route} from 'react-router-dom';
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({match}) => (
-    <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-);
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+    state = {
+        loading: true
+    }
+
+    componentDidMount() {
+        // just for example
+        setTimeout(() => {
+            this.setState({loading: false})
+        }, 1000)
+    }
+
+    render() {
+        const {match} = this.props;
+        const {loading} = this.state;
+
+        return (
+            <div className='shop-page'>
+                <Route
+                    exact
+                    path={`${match.path}`}
+                    render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} /> }
+                />
+                <Route
+                    path={`${match.path}/:collectionId`}
+                    render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} /> }
+                />
+            </div>
+        )
+    }
+}
 
 export default ShopPage;
